94b6edcc064c07c23c03dca8b000bdaf3c5fec7f
carkarof <douglaslaza@gmail.com>
2022-01-17 16:36:26 -0300

    Organizacao da tabela; filtro;

--- Deventos/src/app/eventos/eventos.component.ts
+++ Deventos/src/app/eventos/eventos.component.ts
@@ -8,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any;
+  public eventos : any = [];
+  public eventosFiltrados : any = [];
+  private _filtroLista = '';
+  larguraImagem = 150;
+  margemImagem = 2;
+  exibirImagem = false;
 
   constructor(private http:HttpClient) { }
 
@@ -18,9 +23,29 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
+
+  public get filtroLista():string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value : string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor:string) : any{
+    filtrarPor  = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
 
 }
